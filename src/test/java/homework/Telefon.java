ae576b7f07839d0a3b7c3c7949e03c741e0c395d
gabriel83-30 <gabrielpop83@gmail.com>
2025-05-23 16:27:57 +0300

    add homework requests

---
+++ src/test/java/homework/Telefon.java
@@ -0,0 +1,11 @@
+package homework;
+
+public class Telefon {
+    // de facut OOP ASEMANATOR CU EXEMPLUL DE CURS:
+    // MOSTENIRE,
+    // POLIMORFISM,
+    // INCAPSULARE,
+    // ABSTRACTIZARE(CLASE ABSTRACTE SI INTERFETE),
+    //
+
+}
